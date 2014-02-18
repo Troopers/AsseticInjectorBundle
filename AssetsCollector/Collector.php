ac2a0162886613f9b7ad29fdaaea3e5592aba426
Thomas Beaujean <thomas@appventus.com>
2014-02-18 17:34:31 +0100

    add the feture: the injector json can be override in app/Resources

--- AssetsCollector/Collector.php
+++ AssetsCollector/Collector.php
@@ -10,24 +10,63 @@ use Symfony\Component\Finder\Finder;
  */
 class Collector
 {
+    /**
+     * Constructor
+     *
+     * @param Container $container The container
+     */
     public function __construct($container)
     {
         $this->container = $container;
     }
 
+    /**
+     * Inject the assets
+     *
+     * @param \Twig_TokenParser $parser
+     */
     public function injectAssets(\Twig_TokenParser $parser)
     {
         $parser->addAssets($this->getAssets());
     }
+
+    /**
+     * Get the assets for each bundle
+     *
+     * @return multitype:
+     */
     public function getAssets()
     {
+        //this is the app directory.
+        //the console is in this directory
+        $rootDir = $this->container->get('kernel')->getRootDir();
+        $appOverrideDir = '/Resources';
+
         $resources = array();
         $finder = new Finder();
         $finder->files()->name('assetic_injector.json');
         foreach ($this->container->get('kernel')->getBundles() as $bundle) {
-            if (file_exists($bundle->getPath().'/Resources/config/')) {
-                $finder->in($bundle->getPath().'/Resources/config/');
+            //the number of files found in the override path
+            $finderCount = 0;
+            //the bundle name
+            $bundleName = $bundle->getName();
+
+            //the override path
+            $overridePath = $rootDir.$appOverrideDir.'/'.$bundleName.'/config/';
+
+            //does the file exists in the app/Resources
+            if (file_exists($overridePath)) {
+                $finder->in($overridePath);
+                $finderCount = $finder->count();
             }
+
+            //if no files were found
+            if ($finderCount === 0) {
+                if (file_exists($bundle->getPath().'/Resources/config/')) {
+                    $finder->in($bundle->getPath().'/Resources/config/');
+                }
+            }
+
         }
         $injectArray = array();
         foreach ($finder as $file) {
